24188be1fc3b195a1d34b6c356a7d68852df97f0
Jonathan Digay <jonthan.digay1@gmail.com>
2022-02-20 20:41:05 +0800

    initial

--- components/Layouthead.tsx
+++ components/Layouthead.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Col,
   Container,
@@ -13,8 +13,14 @@ import {
 import Image from "next/image";
 import mindevlogo from "../assets/mindevtran.png";
 import { useRouter } from "next/router";
+import Link from "next/link";
 const Layouthead = () => {
   const path = useRouter().asPath;
+  const [isNav, setIsNav] = useState<boolean>(false);
+
+  const Navigation = () => {
+    setIsNav(!isNav);
+  };
 
   return (
     <Container className="mindev_header" fluid>
@@ -23,8 +29,15 @@ const Layouthead = () => {
           <Image src={mindevlogo} alt="mindev logo" />
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="bars" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className="bars"
+          onClick={Navigation}
+        />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className={`nav-hide ${isNav && "nav-show"}`}
+        >
           <Form className="d-flex search">
             <FormControl
               type="search"
@@ -37,17 +50,29 @@ const Layouthead = () => {
             </Button>
           </Form>
           <ul className="navigations">
-            <li className={`navigation ${path === "/" && "active"}`}>
-              <a href="/">Home</a>
+            <li
+              className={`navigation ${path === "/" && "active"}`}
+              onClick={Navigation}
+            >
+              <Link href="/">Home</Link>
             </li>
-            <li className={`navigation ${path === "/works" && "active"}`}>
-              <a href="/works">Works</a>
+            <li
+              className={`navigation ${path === "/works" && "active"}`}
+              onClick={Navigation}
+            >
+              <Link href="/works">Works</Link>
             </li>
-            <li className={`navigation ${path === "/services" && "active"}`}>
-              <a href="/services">Services</a>
+            <li
+              className={`navigation ${path === "/services" && "active"}`}
+              onClick={Navigation}
+            >
+              <Link href="/services">Services</Link>
             </li>
-            <li className={`navigation ${path === "/about" && "active"}`}>
-              <a href="/about">About us</a>
+            <li
+              className={`navigation ${path === "/about" && "active"}`}
+              onClick={Navigation}
+            >
+              <Link href="/about">About us</Link>
             </li>
           </ul>
         </Navbar.Collapse>
